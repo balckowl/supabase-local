249307e463476d8a452943237f6aa8eaf2422988
balckowl <shiggyjr1492@gmail.com>
2024-10-06 12:04:37 +0900

    init

---
+++ src/db/schema.ts
@@ -0,0 +1,11 @@
+import { pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+
+export const blogsTable = pgTable('blogs_table', {
+    id: serial('id').primaryKey(),
+    title: text('title').notNull(),
+    content: text('content').notNull(),
+    publishedAt: timestamp('published_at').notNull().defaultNow(),
+    updatedAt: timestamp('updated_at').notNull().$onUpdate(() => new Date())
+});
+
+export type SelectBlog = typeof blogsTable.$inferSelect
